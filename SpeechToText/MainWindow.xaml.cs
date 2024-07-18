9c7c9184d5de5cb02625ba66368b51dd6cd181ed
Shaco216 <p.griffin.1911@web.de>
2024-07-18 20:26:41 +0200

    Projektdateien hinzufügen.

---
+++ SpeechToText/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpeechToText
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SpeachRecognizer _speachRecognizer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _speachRecognizer = new SpeachRecognizer();
+        }
+
+        private void start_click(object sender, RoutedEventArgs e)
+        {
+            _speachRecognizer.StartRecording();
+        }
+
+        private void stop_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SpeechToText/SpeachRecognizer.cs
@@ -0,0 +1,42 @@
+﻿using System.Speech.Recognition;
+using System.Windows;
+
+namespace SpeechToText;
+
+public class SpeachRecognizer
+{
+    private readonly SpeechRecognitionEngine _recognizer;
+    public SpeachRecognizer()
+    {
+
+        // Create a new SpeechRecognitionEngine instance
+        _recognizer = new SpeechRecognitionEngine();
+        // Configure the input to the default audio device
+        _recognizer.SetInputToDefaultAudioDevice();
+        // Load a dictation grammar
+        _recognizer.LoadGrammar(new DictationGrammar());
+        // Attach event handlers
+        _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Recognizer_SpeechRecognized);
+    }
+
+    public void StartRecording()
+    {
+        // Start asynchronous recognition
+        _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+        // Keep the console window open
+        //Console.WriteLine("Speak into your microphone.");
+        //Console.ReadLine();
+
+    }
+
+
+
+    static void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+    {
+        // Output the recognized text
+        MessageBox.Show("Recognized text: " + e.Result.Text);
+        //Console.WriteLine("Recognized text: " + e.Result.Text);
+    }
+}
+
