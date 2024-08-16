92da12a7a34428c3c883bd69b589fa87926d3234
Shaco216 <p.griffin.1911@web.de>
2024-08-16 16:13:32 +0200

    klassenanpassung commandInterpreter

--- SpeechToText/CommandInterpreter.cs
+++ SpeechToText/CommandInterpreter.cs
@@ -1,53 +1,52 @@
 ﻿using SpeechToText.Commands;
 
-namespace SpeechToText
+namespace SpeechToText;
+
+public class CommandInterpreter
 {
-    public class CommandInterpreter
+    List<KeyValuePair<string, (WhereNextCommandLocation, string)>>AllCommands = new List<KeyValuePair<string, (WhereNextCommandLocation,string)>>();
+    bool _endOfCommand;
+
+    public static string Trennzeichen { get { return "T"; } }
+    public CommandInterpreter()
     {
-        List<KeyValuePair<string, (WhereNextCommandLocation, string)>>AllCommands = new List<KeyValuePair<string, (WhereNextCommandLocation,string)>>();
-        bool _endOfCommand;
-
-        public static string Trennzeichen { get { return "T"; } }
-        public CommandInterpreter()
+        foreach (var tld in TopLevelCommand.TopLevelDict)
         {
-            foreach (var tld in TopLevelCommand.TopLevelDict)
-            {
-                AllCommands.Add(tld);
-            }
-            foreach(var wcd in WritingCommand.WritingCommandDict)
-            {
-                AllCommands.Add(wcd);
-            }
-            foreach(var ncd in NavigateCommand.NavigateCommandDict)
-            {
-                AllCommands.Add(ncd);
-            }
-            foreach(var mcd in ManualCommand.ManualCommandDict)
-            {
-                AllCommands.Add(mcd);
-            }
+            AllCommands.Add(tld);
         }
-
-        public string Interpret(string spoken)
+        foreach(var wcd in WritingCommand.WritingCommandDict)
         {
-            _endOfCommand = false;
-            string byteCombination = string.Empty;
-            foreach (var word in spoken.Split(' '))
-            {
-                var foundWord =
-                    AllCommands.FirstOrDefault(x => x.Key.Contains(word, StringComparison.OrdinalIgnoreCase));
-                if (foundWord.Key == null || foundWord.Value.Item2 == null || foundWord.Value.Item2 == string.Empty)
-                {
-                    return string.Empty;
-                }
-                (WhereNextCommandLocation w, string binaryData) = foundWord.Value;
-                byteCombination += binaryData;
-                //wurde bereits addiert
-                byteCombination += Trennzeichen;
-
-            }
-            _endOfCommand = true;
-            return byteCombination;
+            AllCommands.Add(wcd);
         }
+        foreach(var ncd in NavigateCommand.NavigateCommandDict)
+        {
+            AllCommands.Add(ncd);
+        }
+        foreach(var mcd in ManualCommand.ManualCommandDict)
+        {
+            AllCommands.Add(mcd);
+        }
+    }
+
+    public string Interpret(string spoken)
+    {
+        _endOfCommand = false;
+        string byteCombination = string.Empty;
+        foreach (var word in spoken.Split(' '))
+        {
+            var foundWord =
+                AllCommands.FirstOrDefault(x => x.Key.Contains(word, StringComparison.OrdinalIgnoreCase));
+            if (foundWord.Key == null || foundWord.Value.Item2 == null || foundWord.Value.Item2 == string.Empty)
+            {
+                return string.Empty;
+            }
+            (WhereNextCommandLocation w, string binaryData) = foundWord.Value;
+            byteCombination += binaryData;
+            //wurde bereits addiert
+            byteCombination += Trennzeichen;
+
+        }
+        _endOfCommand = true;
+        return byteCombination;
     }
 }
